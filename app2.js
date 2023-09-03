ccaed99b612cf558128117ce87aedaa1932c7c2a
syuuma-ito <itosyuuma@gmail.com>
2023-09-03 14:35:23 +0900

    webSocketの接続に対応

--- app2.js
+++ app2.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const WsServer = require("ws").Server;
 const { createHash } = require("crypto");
 const cors = require("cors");
 
@@ -15,12 +16,38 @@ app.get("/", function (request, response) {
     response.sendFile(__dirname + "/index.html");
 });
 
+const wss = new WsServer({ noServer: true });
 const io = new Server(httpServer, {
     cors: {
         origin: "*",
     },
 });
 
+httpServer.removeAllListeners("upgrade");
+httpServer.on("upgrade", (request, socket, head) => {
+    log.info("httpServer upgrade");
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        log.info(`connected`);
+
+        ws.on("message", (message) => {
+            console.log(`WebSocket received: ${message}`);
+        });
+
+        ws.on("close", () => {
+            log.info(`disconnected`);
+        });
+    });
+});
+
+function webSocketBroadcast(data) {
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+}
+
+// socket.io
 io.on("connect", (socket) => {
     log.info("connect : " + socket.id);
     socket.on("disconnect", () => {
@@ -29,13 +56,15 @@ io.on("connect", (socket) => {
 
     socket.on("angles", (angles) => {
         // log.debug("on_angle", angles);
+        webSocketBroadcast(JSON.stringify(angles));
         io.emit("angles", angles);
     });
     socket.on("shoot", (data) => {
         log.debug("shoot", data);
+        webSocketBroadcast(JSON.stringify(data));
         io.emit("shoot", data);
     });
 });
 
 log.info("=== sever start ===");
-httpServer.listen(5500);
+httpServer.listen(5000);
