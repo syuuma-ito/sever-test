824ce949b9f60b522647de7c7389f53ae5c1005d
syuuma-ito <itosyuuma@gmail.com>
2023-09-08 15:22:42 +0900

    パフォーマンス改善

--- app2.js
+++ app2.js
@@ -30,7 +30,7 @@ io.on("connect", (socket) => {
 
     socket.on("angles", (angles) => {
         // TODO sessionIDで現在のユーザーかどうかを判定する
-        io.emit("angles", angles);
+        io.volatile.emit("angles", angles);
     });
     socket.on("shoot", (data) => {
         // TODO sessionIDで現在のユーザーかどうかを判定する
